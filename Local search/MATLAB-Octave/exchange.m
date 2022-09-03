960e676bbd6c4430356de915aa3cd3a64aad7a34
María Candelaria Arpajou <69314665+Candelaria-Arpajou@users.noreply.github.com>
2022-09-03 15:30:59 -0300

    Delete Local search/MATLAB-Octave directory

--- Local search/MATLAB-Octave/exchange.m
+++
@@ -1,8 +0,0 @@
-function [ new_path ] = exchange( path )
-new_path = path;
-indexes = randperm(size(new_path,2),2); % devuelve dos indices random de new_path
-auxiliar = new_path(indexes(1));
-new_path(indexes(1)) = new_path(indexes(2));
-new_path(indexes(2)) = auxiliar;
-end
-
--- Local search/MATLAB-Octave/position_matrix.m
+++
@@ -1,3 +0,0 @@
-function [ position_matrix ] = position_matrix( N )
-position_matrix = rand([2,N]);
-end
